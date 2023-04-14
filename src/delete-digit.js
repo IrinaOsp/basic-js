e7d69633b7bd2ccc3af76396a413236b54740c94
“Irina <irina.osipova2012@gmail.com>
2023-04-14 19:00:07 +0200

    feat: common char.. and delete-digit solved

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = n.toString().split('')
+  let res = []
+  arr.forEach((el, i) => {
+    arr[i] = ''
+    res.push(arr.join(''))
+    arr = n.toString().split('')
+  })
+  return Math.max(...res);
 }
 
 module.exports = {
